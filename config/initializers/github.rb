507cab86b687b4acda3af04c9b6c782521d6db99
Justin Grimes <justinlgrimes@gmail.com>
2013-03-08 23:34:34 -0500

    Updated github api info.

--- config/initializers/github.rb
+++ config/initializers/github.rb
@@ -1,8 +1,7 @@
-@github = Github.new do |config|
+GITHUB = Github.new do |config|
   config.endpoint    = 'https://api.github.com'
   config.adapter     = :net_http
-  config.basic_auth  = ENV['GITHUB_AUTH']
+  config.oauth_token  = "8826a4a2e259c67b9e2b55f6f4c5a5899fb3d702"
   config.ssl         = {:verify => false}
 end
-
-@repo = @github.repos 'BuildThatBillData'
+$repo = BuildThatBill::Application::GITHUB.repos.get 'jgrimes', 'BuildThatBillData'
