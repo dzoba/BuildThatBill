5a628d1103f1c5f724833dbff05f7f4afd2d9dc0
Justin Grimes <justinlgrimes@gmail.com>
2013-03-09 10:05:57 -0500

    Added Problem model.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130309035308) do
+ActiveRecord::Schema.define(:version => 20130309141838) do
+
+  create_table "problems", :force => true do |t|
+    t.string   "title"
+    t.text     "body"
+    t.integer  "number"
+    t.integer  "user_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
