af6128f8ebe24aedbf19c65a3770cd4f2c370b19
Justin Grimes <justinlgrimes@gmail.com>
2013-03-08 21:53:59 -0500

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BuildThatBill::Application.config.secret_token = '818dfb111b859e7bccc62ce3b8ab01fde020db19b2c15bb9637b06f677af59e2567a65a6e7ce02a30aa6e3bc1219917ab245bb67597729feef6e060d41c0e136'
