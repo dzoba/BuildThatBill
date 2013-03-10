8859a2b0c19caa37c8acf0011b3d58887430b506
Justin Grimes <justinlgrimes@gmail.com>
2013-03-09 23:39:23 -0500

    Added user sign up to home page.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,6 +5,17 @@ markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML.new(
         :no_intraemphasis => true, :autolink => true)
     return markdown.render(text).html_safe
   end
+  def resource_name
+    :user
+  end
+
+  def resource
+    @resource ||= User.new
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
 end
 def test_data
   u = User.find(1)
