60f74e00716b384a6f39a391b9f5bc755cf36334
Justin Grimes <justinlgrimes@gmail.com>
2013-03-09 21:02:35 -0500

    Removing github stuff.

--- config/initializers/github.rb
+++ config/initializers/github.rb
@@ -1,4 +1,4 @@
-GITHUB = Github.new oauth_token: "8826a4a2e259c67b9e2b55f6f4c5a5899fb3d702"
+#GITHUB = Github.new oauth_token: "8826a4a2e259c67b9e2b55f6f4c5a5899fb3d702"
 #GITHUB
 #$repo = BuildThatBill::Application::GITHUB.repos 'BuildThatBillData'
 #$git_data = GITHUB.git
